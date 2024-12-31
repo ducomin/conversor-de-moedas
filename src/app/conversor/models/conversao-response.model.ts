a01eef1a177de4b4481dc8c719e123c3b8335cd6
Eduardo Comin <eduardo.comin@capgemini.com>
2024-12-31 15:08:03 -0300

    Aulas 92 - 100 Criação de models.

---
+++ src/app/conversor/models/conversao-response.model.ts
@@ -0,0 +1,8 @@
+export class ConversaoResponse {
+
+    constructor(
+        public base: string,
+        public date: string,
+        public rates: any) {}
+}
+// {"base":"USD","date":"2024-12-31","rates":{"BRL":6.26}}
---
+++ src/app/conversor/models/conversao.model.ts
@@ -0,0 +1,7 @@
+export class Conversao {
+
+    constructor(
+        public modedaDe?: string,
+        public moedaAte?: string,
+        public valor?: number) {}
+}
